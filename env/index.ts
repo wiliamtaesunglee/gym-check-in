dcd37b5bb6a079f0e3f15b5317596b8e41ac4f37
wiliam lee <wiliam.tae@gmail.com>
2024-05-03 09:36:08 -0300

    set up env config

---
+++ env/index.ts
@@ -0,0 +1,17 @@
+import "dotenv/config";
+import { z } from "zod";
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(["dev", "test", "production"]).default("dev"),
+  PORT: z.coerce.number().default(3333),
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (_env.success === false) {
+  console.error("Invalid environment variable");
+
+  throw new Error("Inavlid environment varibales.");
+}
+
+export const env = _env.data;
